7bc470ee5ad71042528b7e3e014b052cf81873cb
Dmitry <df.creative@gmail.com>
2016-08-28 13:45:25 -0400

    Make sync style stream

--- pull-stream.js
+++ pull-stream.js
@@ -1,49 +1,69 @@
-const pull = require('pull-stream');
+const context = require('audio-context');
+const util = require('audio-buffer-utils');
 
-//pull-steam
 
-//a stream of random numbers.
-function source (n) {
-	return function (end, cb) {
-		if(end) return cb(end)
-
-		//only read n times, then stop.
-		if(0 > --n) return cb(true)
-
-		cb(null, Math.random())
-	}
-}
-
-//volume changer
-function through (read, map) {
-	//return a readable function!
-	return function (end, cb) {
-		read(end, function (end, data) {
-			cb(end, data != null ? map(data) : null)
-		});
-	}
-}
-
-//read source and log it.
-function sink () {
-	return function (read) {
-		read(null, function next(end, data) {
-			if(end === true) return
-			if(end) throw end
-
-			console.log(data)
-			read(null, next)
-		});
-	}
+function values (ary) {
+  var i = 0
+  return function read(abort, cb) {
+    if(i===ary.length || abort) return cb(true)
+    cb(null, ary[i++])
+  }
 }
 
 
-// pull(source(), through(), sink());
+function sink (read) {
+  read(null, function next (err, data) {
+    if(err) return console.log(err)
+    console.log(data)
+    //recursively call read again!
+    read(null, next)
+  })
+}
 
-// pull(
-//   pull.values([1, 2, 3]),
-//   pull.asyncMap((v) => v),
-//   pull.collect(function (err, array) {
-//     console.log(array)
-//   })
-// )
+
+function map (mapper) {
+  //a sink function: accept a source
+  return function (read) {
+    //but return another source!
+    return function (abort, cb) {
+      read(abort, function (err, data) {
+        //if the stream has ended, pass that on.
+        if(err) return cb(err)
+        //apply a mapping to that data
+        cb(null, mapper(data))
+      })
+    }
+  }
+}
+
+var source = values([1,2,3])
+var mapper = map(function (e) { return e*e })
+
+
+function pull () {
+  var args = [].slice.call(arguments)
+  var s = args.shift()
+  while(args.length) s = args.shift()(s)
+  return s
+}
+
+function infinite () {
+  var i = 0
+  return function (abort, cb) {
+    if(abort) return cb(abort)
+    cb(null, i++)
+  }
+}
+
+function take (n) {
+  return function (read) {
+    return function (abort, cb) {
+      //after n reads, tell the source to abort!
+      if(!n--) return read(true, cb)
+      read(null, cb)
+    }
+  }
+}
+
+
+pull(infinite(), mapper, take(101), sink)
