f86dd6180840c2909e9ad69236ef69c456087bc0
Dmitry <df.creative@gmail.com>
2017-02-10 09:36:15 +0300

    Add contestant

--- pull-stream-to-stream.js
+++ pull-stream-to-stream.js
@@ -8,76 +8,75 @@ const now = require('right-now');
 
 
 let duration = 5000;
-
 let frameSize = 1024;
 
-// function sine () {
-//   return pull.infinite(function () {
-//     return util.noise(util.create(frameSize))
-//   })
-// }
 
-// function volume () {
-//   return pull.map(function (data) {
-//     util.fill(data, v => v * .01);
-//     return data
-//   })
-// }
+//state holders for speaker - a scheduled data and release callback.
+let readyData, release;
 
 
-//Streams
-const Readable = require('stream').Readable;
-const Transform = require('stream').Transform;
+let sum = 0, count = 0;
 
-let sine = Readable({
-  highWaterMark: 0,
-  objectMode: true,
-  read: function (size) {
-    this.push(util.noise(util.create(frameSize)));
-  }
+//create speaker routine
+let bufferNode = context.createBufferSource();
+bufferNode.loop = true;
+bufferNode.buffer = util.create(2, frameSize);
+let buffer = bufferNode.buffer;
+let node = context.createScriptProcessor(frameSize);
+node.addEventListener('audioprocess', function (e) {
+  let t = now();
+  count++;
+
+  release && release();
+  /*
+  Here we release writable’s callback, which instantly "pulls" the whole stream chain synchronously.
+   */
+
+  util.copy(readyData || e.inputBuffer, e.outputBuffer);
+  sum += (now() - t);
 });
+bufferNode.connect(node);
+node.connect(context.destination);
+bufferNode.start();
 
-let volume = Transform({
-  highWaterMark: 0,
-  objectMode: true,
-  transform: (chunk, enc, cb) => {
-    util.fill(chunk, v => v * .01);
-    cb(null, chunk);
-  }
-});
+
+
+function sine () {
+  return pull.infinite(function () {
+    return util.noise(util.create(frameSize))
+  })
+}
+
+function volume () {
+  return pull.map(function (data) {
+    util.fill(data, v => v * .01);
+    return data
+  })
+}
 
 
 //create speaker routine
 function speaker () {
   return function (read) {
-    let sum = 0, count = 0;
-
-    var bufferNode = context.createBufferSource()
-    bufferNode.loop = true;
-    bufferNode.buffer = util.create(2, frameSize)
-    var node = context.createScriptProcessor(frameSize)
-    node.addEventListener('audioprocess', function (e) {
-      let t = now();
-      count++;
-
       read(null, function (err, data) {
-        util.copy(data, e.outputBuffer)
-        sum += (now() - t);
-      })
-    });
-    bufferNode.connect(node)
-    node.connect(context.destination)
-    bufferNode.start();
-
-
-    setTimeout(() => {
-  console.log(`Pull stream: ${sum}ms for ${count} frames, average ${sum/count}ms per frame`);
-
-      node.disconnect();
-      read(true, () => {});
-    }, duration);
+        util.copy(data, e.outputBuffer);
+      });
   }
 }
+
+let read = speaker();
+
+
+let speaker = Writable({
+  highWaterMark: 0,
+  objectMode: true,
+  write: (chunk, enc, cb) => {
+    read();
+    readyData = chunk;
+    release = cb;
+  }
+});
+
 
 // pull(sine(), volume(), speaker())
 sine.pipe(volume).pipe(toStream.sink(speaker));
